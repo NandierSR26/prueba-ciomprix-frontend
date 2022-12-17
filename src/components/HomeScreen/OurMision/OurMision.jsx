d1bd686ad70442de616ebc0017d4c2b713d873a5
Nandier Ruiz <nandiersruiz@outlook.com>
2022-12-17 00:59:29 -0500

    feat: :art: Our mission section finished

---
+++ src/components/HomeScreen/OurMision/OurMision.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const OurMision = () => {
+    return (
+        <section className="our__mission w-full ss:w-[65%] flex flex-col items-center gap-7 my-0 mx-auto py-[5rem]">
+            <h2 className="text-3xl text-text">Our Mission</h2>
+            <p className="text-center text-text2 px-5 lg:px-12">
+                Founded in 2014, Achoo believes that influencers including bloggers, are the best digital
+                marketing channel. We help advertisers to engage and execute efficient influencer mar-
+                keting strategies. Our digital platform with turnkey analytics and reporting features based
+                on real-time influencer data provides advertisers overview and transparency into the
+                vastly unstructured influencer market.
+            </p>
+        </section>
+    )
+}
--- src/pages/HomeScreen/Home.jsx
+++ src/pages/HomeScreen/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Hero } from '../../components/HomeScreen/Hero/Hero'
+import { OurMision } from '../../components/HomeScreen/OurMision/OurMision'
 
 export const Home = () => {
     return (
-        <Hero />
+        <>
+            <Hero />
+            <OurMision />
+        </>
     )
 }
